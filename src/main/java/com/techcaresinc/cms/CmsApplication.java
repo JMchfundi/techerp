0c4001b3f6a1c19ff21fcc5c6f54fe7477f60c0c
JMchfundi <akidamjaffar@gmail.com>
2021-01-15 14:40:08 -0500

    fragments

--- src/main/java/com/techcaresinc/cms/CmsApplication.java
+++ src/main/java/com/techcaresinc/cms/CmsApplication.java
@@ -4,14 +4,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class CmsApplication extends SpringBootServletInitializer {
 
     @RequestMapping(value = "/")
     public String root (){
         return "index";
+    }
+
+    @RequestMapping(value = "/form_component")
+    public String form_component(){
+        return "form_component";
+    }
+
+    @RequestMapping(value = "/form_validation")
+    public String form_validation(){
+        return "form_validation";
+    }
+
+    @RequestMapping(value = "/general")
+    public String general(){
+        return "general";
+    }
+
+    @RequestMapping(value = "/buttons")
+    public String buttons(){
+        return "buttons";
+    }
+
+    @RequestMapping(value = "/grids")
+    public String grids(){
+        return "grids";
     }
 
     public static void main(String[] args) {
